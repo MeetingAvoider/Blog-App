cb724466106cc1e5a0b9a30732da4aa2aa709980
Sameena99786 <nitinhansraj102@gmail.com>
2024-04-26 22:39:40 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config(); // Load environment variables from .env file
+console.log("MongoDB connection string:", process.env.MONGO);
+
+mongoose
+  .connect(
+    process.env.MONGO // Use the MONGO environment variable
+  )
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.error("Error connecting to database:", err);
+  });
+
 const app = express();
 app.listen(9000, () => {
-  console.log("hello world");
+  console.log("Server is running on port 9000");
 });
